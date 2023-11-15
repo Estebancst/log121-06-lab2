c211b183d043f531b3e0009e59d7cb6b8c1aa8c1
CurePaula <pau.an.cure@gmail.com>
2023-11-15 14:10:38 -0500

    main
    main

--- src/demarrageApp/Main.java
+++ src/demarrageApp/Main.java
@@ -1,8 +1,12 @@
 package demarrageApp;
 
+import view.FenetrePrincipale;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        FenetrePrincipale fenetre = new FenetrePrincipale();
+        
     }
 
 }
